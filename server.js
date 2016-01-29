2e30480fda0dd36fe27740919f5f03846dd19122
cfreeland <diathis47@gmail.com>
2016-01-28 21:41:13 -0800

    refactored with underscore

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
 var todos = [];
@@ -12,15 +14,11 @@ app.use(bodyParser.json());
 app.get('/todos', function (req, res) {
     res.json(todos);
 });
+
 //get /todos/:id
 app.get('/todos/:id', function (req, res) {
    var todoId = parseInt(req.params.id, 10);
-   var matchedTodo;
-   todos.forEach(function (todo) {
-        if (todoId === todo.id) {
-            matchedTodo = todo;
-        }
-   });
+   var matchedTodo = _.findWhere(todos, {id: todoId});
     
    if (matchedTodo) {
        res.json(matchedTodo);
@@ -32,16 +30,19 @@ app.get('/todos/:id', function (req, res) {
 
 //POST request
 app.post('/todos', function (req, res) {
-    var body = req.body;
+    var body = _.pick(req.body,'description', 'completed');
     
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+    
+    // set body.description to the trimmed value
+    body.description = body.description.trim();
     
     body.id = todoNextId++;
     todos.push(body);
     res.json(body);
-    
 });
-
-
 
 app.listen(PORT, function () {
     console.log('express listening on port ' + PORT);
